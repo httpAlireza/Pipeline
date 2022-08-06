3bfdf05e0ded592f31ac1a6406920ce68e3a8068
httpAlireza <Alirezakh2866@gmail.com>
2022-08-06 12:35:20 +0430

    some minor changes in Handler calss

--- src/main/java/ir/jibit/handler/Handler.java
+++ src/main/java/ir/jibit/handler/Handler.java
@@ -1,7 +1,7 @@
 package ir.jibit.handler;
 
 /**
- * An abstract class witch has only one functioning method witch takes a single argument and returns the result.
+ * An abstract class which has only one functioning method which takes a single argument and returns the result.
  *
  * @param <I> the type of the input to the operation
  * @param <O> the type of te output of function
@@ -26,9 +26,9 @@ public abstract class Handler<I, O> {
     }
 
     /**
-     * The abstract method witch should be overridden for the purpose.
+     * The abstract method which should be overridden for the purpose.
      *
-     * @param input the object witch enters to the handler.
+     * @param input the object which enters to the handler.
      * @return an object of O type.
      */
     public abstract O function(I input);
