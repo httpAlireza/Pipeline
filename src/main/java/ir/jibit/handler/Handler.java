de05099d575c3adf0a9f25864aa537ccc733b094
httpAlireza <Alirezakh2866@gmail.com>
2022-08-06 12:27:30 +0430

    Handler interface changed to Handler abstract class and handler id added to it.

--- src/main/java/ir/jibit/handler/Handler.java
+++ src/main/java/ir/jibit/handler/Handler.java
@@ -1,12 +1,32 @@
 package ir.jibit.handler;
 
 /**
- * Represents an operation that accepts a single input argument and returns an object
+ * An abstract class witch has only one functioning method witch takes a single argument and returns the result.
  *
  * @param <I> the type of the input to the operation
  * @param <O> the type of te output of function
+ * @author Alireza khodadoust
  */
-@FunctionalInterface
-public interface Handler<I, O> {
-    O function(I input);
+
+public abstract class Handler<I, O> {
+    /**
+     * Saves a string as id for the handler.
+     */
+    private String handlerId;
+
+    public String getHandlerId() {
+        return handlerId;
+    }
+
+    public void setHandlerId(String handlerId) {
+        if (handlerId == null || handlerId.isBlank()) {
+            throw new IllegalArgumentException("handler id can not be null or blank!");
+        }
+        this.handlerId = handlerId;
+    }
+    /** The abstract method witch should be overridden for the purpose.
+     * @param input the object witch enters to the handler.
+     * @return an object of O type.
+     */
+    public abstract O function(I input);
 }
